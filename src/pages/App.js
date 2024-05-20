8d402c626b7e4f0b8cadc0df1391535ce20a8d56
vitor037 <vitoraugusto0035@gmail.com>
2024-05-20 11:29:00 -0300

    Criação da Roleta

---
+++ src/pages/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import WheelComponent from '../components/wheel';
+import '../index.css';
+import LogoVarejo from '../assets/Logo_Varejo.png';
+import LogoFocus from '../assets/Logo_Focus.png';
+import ClickIcon from '../assets/Click_Icon.jpeg';
+import MarcaSebrae from '../assets/marca-sebrae.png';
+import LogoToner from '../assets/LogoToner.png';
+
+import { useNavigate } from 'react-router-dom';
+
+const App = () => {
+  const navigate = useNavigate();
+
+  const segments = ['Varejo', 'Logística', 'Operacional'];
+  const segColors = ['#faa230', '#055cba', '#bb1625'];
+  const onFinished = (winner) => {
+    //console.log(winner);
+    setTimeout(() => {
+      navigate(`/${winner}`);
+    }, 600);
+  };
+
+  let rotacao = Math.floor(Math.random() * 500);
+
+  if (rotacao < 150) {
+    rotacao = 150;
+  }
+
+  console.log(rotacao);
+
+  const responsive = false;
+
+  return (
+    <>
+      <div className="main_text">
+        <p>Varejo+ Experience</p>
+      </div>
+      {responsive ? (
+        ''
+      ) : (
+        <img className="icone-click" src={ClickIcon} alt="Ícone de Clique" />
+      )}
+
+      <WheelComponent
+        segments={segments}
+        segColors={segColors}
+        onFinished={(winner) => onFinished(winner)}
+        primaryColor="#FCF4F7"
+        contrastColor="white"
+        buttonText=""
+        isOnlyOnce={false}
+        size={responsive ? 100 : 290}
+        upDuration={rotacao}
+        downDuration={1200}
+        fontFamily="Arial"
+      />
+      <div className="left_img">
+        <img src={LogoVarejo} alt="Logo Varejo" />
+      </div>
+      <div className="right_img">
+        <img src={LogoFocus} alt="Logo Sebrae" />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/perguntas.js
@@ -0,0 +1,477 @@
+export const Perguntas = [
+  {
+    tema: 'Operacional',
+    pergunta:
+      'Ao vender em Marketplaces é importante ter mais de um anúncio, com títulos diferentes, para o mesmo produto porque:',
+    alternativas: [
+      {
+        texto:
+          'Issó possibilita trabalhar com objetivos de buscas diferentes, abrangendo varias formas de pesquisa.',
+        valor: true,
+      },
+      {
+        texto:
+          'Para os consumidores acharem que o produto vende muito e assim vender mais',
+        valor: false,
+      },
+      {
+        texto:
+          'Faz com que o cliente queira comprar o mesmo produto mais de uma vez',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Operacional',
+    pergunta:
+      'O investimento em ADS, mesmo vendendo nos Marketplaces, é importante porque:',
+    alternativas: [
+      {
+        texto:
+          'A publicidade hoje é necessaria pois com ela ampliamos os resultados de buscas onde os nossós anúncios aparecem.',
+        valor: true,
+      },
+      {
+        texto: 'Aumenta os gastos da empresa sem trazer muitos beneficios',
+        valor: false,
+      },
+      {
+        texto: 'Não é importante se você estiver vendendo em Marketplaces',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Operacional',
+    pergunta: 'Em relação ão titulo de um anúncio ele deve:',
+    alternativas: [
+      {
+        texto:
+          'Conter o nome, modelo, caracteristicas é marca. Todas essas informações ajudam a especificar seu produto é aparecer para as pessóas.',
+        valor: true,
+      },
+      {
+        texto: 'Ser uma promessa falsa, para capturar a atenção do consumidor',
+        valor: false,
+      },
+      {
+        texto:
+          'Ser curto e incompleto, para que o cliente fique curioso e queira comprar',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Operacional',
+    pergunta: 'Uma boa descrição do seu produto é importante porque?',
+    alternativas: [
+      {
+        texto:
+          'A descrição sera um dos principais pontos para melhorar o SeO nos mecanismos de busca, issó porque nela estara presente tanto a palavra-chave como demais informações que podem potencializar as buscas.',
+        valor: true,
+      },
+      { texto: 'E na descrição onde você atrai mais clientes.', valor: false },
+      { texto: 'A descrição valoriza o valor do produto', valor: false },
+    ],
+  },
+  {
+    tema: 'Operacional',
+    pergunta:
+      'Ter boas fotos com iluminação adequada, qualidade alta, ambientação é diversós angulos, é importante porque:',
+    alternativas: [
+      {
+        texto: 'Ajudam o cliente a decidir se realizara a compra.',
+        valor: true,
+      },
+      { texto: 'Faz os clientes perderem o interesse', valor: false },
+      {
+        texto: 'Confunde o consumidor na hora de realizar a venda',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Operacional',
+    pergunta: 'Oferecer um prazo de entrega otimizado é importante porque:',
+    alternativas: [
+      {
+        texto:
+          'Quem compra quer receber o seu produto no menor tempo possivel é issó é um dos fatores de decisão de compra.',
+        valor: true,
+      },
+      {
+        texto: 'As vendas não são influenciadas pelo prazo de entrega',
+        valor: false,
+      },
+      {
+        texto: 'Não é um fator de decisão de compra do consumidor',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Operacional',
+    pergunta:
+      'O Frete Gratis pode ser uma estratégia interessante para alavancar as vendas porque:',
+    alternativas: [
+      {
+        texto:
+          'Entregas gratuitas são um grande incentivo para atrair compradores as suas publicações.',
+        valor: true,
+      },
+      {
+        texto: 'Entregas gratis só atraem um unico tipo de comprador',
+        valor: false,
+      },
+      {
+        texto: 'É melhor oferecer frete gratis do que desconto nas vendas',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Operacional',
+    pergunta:
+      'Entender as regras dos Marketplaces que atua é importante porque?',
+    alternativas: [
+      {
+        texto:
+          'Casó descumpra as regras para os anúncios você podera ser penalizado, chegando ate a ter a conta bloqueada, ficando impossibilitado de vender naquele Marketplace.',
+        valor: true,
+      },
+      {
+        texto: 'Para denunciar outros anúncios e assim eliminar concorrência',
+        valor: false,
+      },
+      {
+        texto: 'Para saber quais regras deve descumprir sem ser bloqueado',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Varejo',
+    pergunta:
+      'Na hora de decidir se fecha uma compra ou não, o consumidor sempre avalia o valor do frete e o prazo de entrega. Isso impacta nas vendas porque:',
+    alternativas: [
+      {
+        texto:
+          'Contar com tarifas altas ou tempo de entrega extenso faz com que as pessóas evitem seus produtos.',
+        valor: true,
+      },
+      {
+        texto: 'O valor do frete é mais importante que o do produto',
+        valor: false,
+      },
+      {
+        texto: 'Para comprar em outro lugar sem pagar o frete.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Varejo',
+    pergunta:
+      'Analisar os precos praticados pela concorrência é produtos que estao “em alta” te ajudara?',
+    alternativas: [
+      {
+        texto:
+          'Sim, ajudara a identificar muitas oportunidades de melhorar seu mix, de melhorar a oferta de produtos.',
+        valor: true,
+      },
+      {
+        texto: 'Sim, para falar mal dos concorrentes para os clientes.',
+        valor: false,
+      },
+      {
+        texto: 'Não, tenha seus proprios precos e produtos.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Varejo',
+    pergunta:
+      'No perfil do @varejomais.sebrae no Instagram, você encontrará muita informação sobre:',
+    alternativas: [
+      {
+        texto: 'Como se destacar vendendo on-line e off-line',
+        valor: true,
+      },
+      {
+        texto: 'Como se destacar vendendo somente on-line',
+        valor: false,
+      },
+      {
+        texto: 'Como se destacar vendendo somente off-line',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Varejo',
+    pergunta: 'Conquistar boas avaliações é importante porque:',
+    alternativas: [
+      {
+        texto:
+          'Apos observar preco, condição de pagamento e frete, os compradores tendem a consultar a opiniao de outras pessoas para saber a experiência delas tanto no ato da compra quanto com o uso do produto.',
+        valor: true,
+      },
+      {
+        texto: 'Seu negócio fica mais conhecido pelos seus clientes',
+        valor: false,
+      },
+      {
+        texto:
+          'Possibilita vender produtos de menor qualidade por um preco mais alto, ja que as avaliações são boas.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Varejo',
+    pergunta:
+      'Na hora de decidir se fecha uma compra ou não, o consumidor sempre avalia o valor do frete e o prazo de entrega porque?',
+    alternativas: [
+      {
+        texto:
+          'Contar com tarifas altas ou tempo de entrega extenso faz com que as pessóas evitem seus produtos.',
+        valor: true,
+      },
+      {
+        texto: 'O valor do frete é mais importante que o do produto.',
+        valor: false,
+      },
+      {
+        texto: 'Para comprar em outro lugar sem pagar o frete',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Varejo',
+    pergunta: 'É um ponto extremamente importante na descrição dos produtos:',
+    alternativas: [
+      {
+        texto:
+          'Uso de palavras-chave no titulo e na descrição, pois as Palavras-chave são os termos mais usados pelos usuarios para pesquisar sobre um produto.',
+        valor: true,
+      },
+      {
+        texto: 'Todas as informações técnicas possiveis sobre o produto.',
+        valor: false,
+      },
+      {
+        texto: 'Informações sobre outros produtos da loja.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Varejo',
+    pergunta: 'Para ter mais visualizações dentro de um marketplace você pode?',
+    alternativas: [
+      {
+        texto: 'Anunciar dentro do marketplace onde vende',
+        valor: true,
+      },
+      {
+        texto: 'Comentar em anúncios de outras pessóas',
+        valor: false,
+      },
+      {
+        texto: 'Fazer anúncios que não condizem com a realidade',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Varejo',
+    pergunta:
+      'Em relação as perguntas feitas pelos clientes on-line você deve:',
+    alternativas: [
+      {
+        texto:
+          'Responder rapidamente e cordialmente, pois as perguntas são uma fonte de venda riquissima, não deixe de ser proativo e responder pelo menos no intervalo de 1h.',
+        valor: true,
+      },
+      {
+        texto:
+          'Responder apenas as que corresponderem a duvidas sobre compras.',
+        valor: false,
+      },
+      {
+        texto:
+          'Esperar acumular perguntas para responder varias de uma vez, otimizando seu tempo.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Logística',
+    pergunta:
+      'Ao vender em Marketplaces é importante ter mais de um anúncio, com títulos diferentes, para o mesmo produto porque:',
+    alternativas: [
+      {
+        texto:
+          'Isso possibilita trabalhar com objetivos de buscas diferentes, abrangendo várias formas de pesquisa.',
+        valor: true,
+      },
+      {
+        texto:
+          'Para os consumidores acharem que o produto vende muito e assim vender mais',
+        valor: false,
+      },
+      {
+        texto:
+          'Faz com que o cliente queira comprar o mesmo produto mais de uma vez',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Logística',
+    pergunta:
+      'Apura indicadores de vendas como: Ticket Médio; Preco Médio e Taxa de conversão é importante porque:',
+    alternativas: [
+      {
+        texto:
+          'Apurar os indicadores de vendas permitirá que você faca a projeção dos resultados mediante a realização de uma promoção de descontos para atrair mais clientes por exemplo, permitindo avaliar se a promoção será vantajosa ou não para o negócio.',
+        valor: true,
+      },
+      {
+        texto:
+          'Ao apurar os indicadores de vendas o empresario podera planejar melhor seu fluxo de caixa, evitando perdas com despesas financeiras desnecessarias.',
+        valor: false,
+      },
+      {
+        texto:
+          'Pensando mais a longo prazo, os indicadores de vendas poderao ser dissociados de todos os recursos disponiveis na empresa, desde que se tenha um planejamento estratégico consistente.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Logística',
+    pergunta:
+      'Porque é importante segmentar seu publico-alvo para o definir o posicionamento do negócio?',
+    alternativas: [
+      {
+        texto:
+          'Ao segmentar seu mercado, você consegue entender com maior profundidade quem são seus potenciais clientes, bem como qual é a melhor forma de abordá-los, tornando a comunicação mais fácil e assertiva.',
+        valor: true,
+      },
+      {
+        texto:
+          'A segmentação é importante porque com ela você podera atender a varios pubicos diferentes, ampliando o universo de clientes do negocio.',
+        valor: false,
+      },
+      {
+        texto:
+          'Segmentar seu público alvo ajudará a definir estratégias de marketing para as divulgações em massa que atendam a todas as pessoas.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Logística',
+    pergunta:
+      'Promover programa de relacionamento com vantagens e benefícios diferentes de clientes anônimos?',
+    alternativas: [
+      {
+        texto:
+          'Issó podera aumentar a percepção do cliente de vantagens é beneficios, contribuindo para o aumento de vendas é fidelização.',
+        valor: true,
+      },
+      {
+        texto:
+          'Isso possibilitará eu se conheca os clientes mais chatos e que devem ser evitados pelos vendedores.',
+        valor: false,
+      },
+      {
+        texto:
+          'Issó permitira a realização de campanhas promocionais com foco em novos clientes.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Logística',
+    pergunta:
+      'Em relação as perguntas feitas pelos clientes on-line você deve:',
+    alternativas: [
+      {
+        texto:
+          'Responder rapidamente e cordialmente, pois as perguntas são uma fonte de venda riquíssima, não deixe de ser proativo e responder pelo menos no intervalo de 1h.',
+        valor: true,
+      },
+      {
+        texto: 'Responder apenas as que corresponderem a dúvidas sobre compras',
+        valor: false,
+      },
+      {
+        texto:
+          'Esperar acumular perguntas para responder várias de uma vez, otimizando seu tempo.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Logística',
+    pergunta: 'É importante caprichar na descrição dos produtos pois:',
+    alternativas: [
+      {
+        texto:
+          'Colocando o maximo de informações possivel nos anúncios o cliente tera mais seguranca sobre o que esta comprando e reduzirá a quantidade de devoluções.',
+        valor: true,
+      },
+      {
+        texto:
+          'Quanto mais formações na descrição do anúncio mais valorizado é o seu produto.',
+        valor: false,
+      },
+      {
+        texto: 'Uma descrição rasa e direta aumenta as vendas.',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Logística',
+    pergunta: 'O que utilizar para aumenta a atratividade de um anúncio?',
+    alternativas: [
+      {
+        texto: 'Fotos bem elaboradas.',
+        valor: true,
+      },
+      {
+        texto: 'Preços fora do padrão do mercado',
+        valor: false,
+      },
+      {
+        texto: 'Falsas promessas para os clientes',
+        valor: false,
+      },
+    ],
+  },
+  {
+    tema: 'Logística',
+    pergunta:
+      'Por que é importante ficar atento as perguntas e respondê-las no menor prazo possivel ?',
+    alternativas: [
+      {
+        texto:
+          'Porque isso diminui a chance de o cliente buscar outro vendedor.',
+        valor: true,
+      },
+      {
+        texto: 'Para ter mais tempo livre.',
+        valor: false,
+      },
+      {
+        texto: 'Pois quanto menor o contato com o cliente, melhor.',
+        valor: false,
+      },
+    ],
+  },
+];
